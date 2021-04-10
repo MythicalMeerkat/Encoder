210114f0c179d4047f2b8075bb811eeac679e5a2
Jeff Wilson <MythicalMeerkat@users.noreply.github.com>
2021-04-10 00:52:51 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ def two_level_gray_encoding(imageArr, width, height):
     for x in range(width):
         for y in range(height):
             if 0 <= imageArr[x, y] <= 127:
-                # print('Pixel: ' + str(x) + ', ' + str(y) + ' set to 0! \n')
                 imageArr[x, y] = 0
-                # fout.write(bytearray(0))
             elif 128 <= imageArr[x, y] <= 255:
-                # print('Pixel: ' + str(x) + ', ' + str(y) + ' set to 1! \n')
                 imageArr[x, y] = 1
-                # fout.write(bytearray(1))
 
 
 def two_level_gray_decoding(imageArr, width, height):
@@ -29,6 +25,78 @@ def two_level_gray_decoding(imageArr, width, height):
                 imageArr[x, y] = 63
             elif imageArr[x, y] == 1:
                 imageArr[x, y] = 191
+
+
+def fifteen_level_gray_encoding(imageArr, width, height):
+    # Number of Pixels in the image
+    # Check each pixel in the numpy array. Split into the two gray levels
+    for x in range(width):
+        for y in range(height):
+            if 0 <= imageArr[x, y] <= 16:
+                imageArr[x, y] = 0
+            elif 17 <= imageArr[x, y] <= 34:
+                imageArr[x, y] = 1
+            elif 35 <= imageArr[x, y] <= 52:
+                imageArr[x, y] = 2
+            elif 53 <= imageArr[x, y] <= 70:
+                imageArr[x, y] = 3
+            elif 71 <= imageArr[x, y] <= 88:
+                imageArr[x, y] = 4
+            elif 89 <= imageArr[x, y] <= 106:
+                imageArr[x, y] = 5
+            elif 107 <= imageArr[x, y] <= 124:
+                imageArr[x, y] = 6
+            elif 125 <= imageArr[x, y] <= 142:
+                imageArr[x, y] = 7
+            elif 143 <= imageArr[x, y] <= 160:
+                imageArr[x, y] = 8
+            elif 161 <= imageArr[x, y] <= 178:
+                imageArr[x, y] = 9
+            elif 179 <= imageArr[x, y] <= 196:
+                imageArr[x, y] = 10
+            elif 197 <= imageArr[x, y] <= 214:
+                imageArr[x, y] = 11
+            elif 215 <= imageArr[x, y] <= 232:
+                imageArr[x, y] = 12
+            elif 233 <= imageArr[x, y] <= 250:
+                imageArr[x, y] = 13
+            elif 251 <= imageArr[x, y] <= 255:
+                imageArr[x, y] = 14
+
+
+def fifteen_level_gray_decoding(imageArr, width, height):
+    for x in range(width):
+        for y in range(height):
+            if imageArr[x, y] == 0:
+                imageArr[x, y] = 8
+            elif imageArr[x, y] == 1:
+                imageArr[x, y] = 25
+            elif imageArr[x, y] == 2:
+                imageArr[x, y] = 43
+            elif imageArr[x, y] == 3:
+                imageArr[x, y] = 61
+            elif imageArr[x, y] == 4:
+                imageArr[x, y] = 79
+            elif imageArr[x, y] == 5:
+                imageArr[x, y] = 97
+            elif imageArr[x, y] == 6:
+                imageArr[x, y] = 115
+            elif imageArr[x, y] == 7:
+                imageArr[x, y] = 133
+            elif imageArr[x, y] == 8:
+                imageArr[x, y] = 151
+            elif imageArr[x, y] == 9:
+                imageArr[x, y] = 169
+            elif imageArr[x, y] == 10:
+                imageArr[x, y] = 177
+            elif imageArr[x, y] == 11:
+                imageArr[x, y] = 205
+            elif imageArr[x, y] == 12:
+                imageArr[x, y] = 223
+            elif imageArr[x, y] == 13:
+                imageArr[x, y] = 241
+            elif imageArr[x, y] == 14:
+                imageArr[x, y] = 253
 
 
 # Read .pgma image into Numpy Array
@@ -47,6 +115,7 @@ print('Type: ' + str(type(image_original)) + '\n')
 print('Shape: ' + str(image_original.shape) + '\n')
 
 # Start 2 Gray Level Encoding Process
+
 # Generate Header
 max_value = 1
 pgmHeader = 'P5' + '\n' + str(width) + ' ' + str(height) + '\n' + str(max_value) + '\n' + '\n'
@@ -62,22 +131,62 @@ cv2.imwrite(filename, image_2_Gray)
 # Replace header in generated .pgm file
 with open(filename, "r+b") as file:
     file.write(pgmHeader_byte)
-
 file.close()
 
 # Start 2 Gray Level Decoding Process
+
+# Generate Header
 max_value = 191
 pgmHeader = 'P5' + '\n' + str(width) + ' ' + str(height) + '\n' + str(max_value) + '\n' + '\n'
 pgmHeader_byte = bytearray(pgmHeader, 'utf-8')
 filename = "baboon2-levels-R.pgm"
 
+# Decode
 two_level_gray_decoding(image_2_Gray, width, height)
 print('Printing Image: ' + str(filename))
 cv2.imwrite(filename, image_2_Gray)
 
+# Replace header in generated .pgm file
 with open(filename, "r+b") as file:
     file.write(pgmHeader_byte)
+file.close()
 
+
+# Start 15 Gray Level Encoding Process
+
+# Generate Header
+max_value = 14
+pgmHeader = 'P5' + '\n' + str(width) + ' ' + str(height) + '\n' + str(max_value) + '\n' + '\n'
+pgmHeader_byte = bytearray(pgmHeader, 'utf-8')
+filename = "baboon15-levels.pgm"
+
+# Encode
+fifteen_level_gray_encoding(image_15_Gray, width, height)
+array_out = numpy.array(image_15_Gray)
+print('Printing Image: ' + str(filename))
+cv2.imwrite(filename, image_15_Gray)
+
+# Replace header in generated .pgm file
+with open(filename, "r+b") as file:
+    file.write(pgmHeader_byte)
+file.close()
+
+# Start 15 Gray Level Decoding Process
+
+# Generate Header
+max_value = 253
+pgmHeader = 'P5' + '\n' + str(width) + ' ' + str(height) + '\n' + str(max_value) + '\n' + '\n'
+pgmHeader_byte = bytearray(pgmHeader, 'utf-8')
+filename = "baboon15-levels-R.pgm"
+
+# Decode
+fifteen_level_gray_decoding(image_15_Gray, width, height)
+print('Printing Image: ' + str(filename))
+cv2.imwrite(filename, image_15_Gray)
+
+# Replace header in generated .pgm file
+with open(filename, "r+b") as file:
+    file.write(pgmHeader_byte)
 file.close()
 
 
